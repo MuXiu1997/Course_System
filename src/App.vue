d0db0714ed18e1e56b0f559878b623a28fdd3391
邹昆峰 <49554020+MuXiu1997@users.noreply.github.com>
2019-06-03 23:24:48 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  /*#app {*/
+  /*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*  -webkit-font-smoothing: antialiased;*/
+  /*  -moz-osx-font-smoothing: grayscale;*/
+  /*  text-align: center;*/
+  /*  color: #2c3e50;*/
+  /*}*/
+  /*#nav {*/
+  /*  padding: 30px;*/
+  /*  a {*/
+  /*    font-weight: bold;*/
+  /*    color: #2c3e50;*/
+  /*    &.router-link-exact-active {*/
+  /*      color: #42b983;*/
+  /*    }*/
+  /*  }*/
+  /*}*/
+  /*.ivu-input[disabled], fieldset[disabled] .ivu-input {*/
+  /*  background-color: #fff !important;*/
+  /*  color: #000 !important;*/
+  /*}*/
+
+  /*.el-input-number__decrease, .el-input-number__increase {*/
+  /*  background-color: #fff !important;*/
+  /*}*/
+</style>
