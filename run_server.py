325a23d9534ecd6e7f45623dfd23fced98fb5007
邹昆峰 <49554020+MuXiu1997@users.noreply.github.com>
2019-08-27 21:17:14 +0800

    feat: 服务器启动脚本

--- run_server.py
+++ run_server.py
@@ -1,16 +1,34 @@
+# -*- coding: utf-8 -*-
 from subprocess import call
 
-call(['docker', 'rm', '-f', 'Course_System_flask_uwsgi'])
+COURSE_SYSTEM = '/root/Course_System_v20190827/'
+C_DJANGO = 'Course_System_django_v20190827'
+C_NGINX = 'Course_System_nginx_v20190827'
 
-call(['docker', 'rm', '-f', 'Course_System_nginx'])
 
-call(['docker', 'run', '-d', '-p', '80:80', '--name', 'Course_System_nginx',
-      '-v', '/root/Course_System/Front_end_Vue:/app/html',
-      '-v', '/root/Course_System/config/nginx.conf:/etc/nginx/nginx.conf',
-      '-v', '/root/Course_System/logs:/var/log/nginx',
-      'nginx:1.16'])
+def rm():
+    call(['docker', 'rm', '-f', C_DJANGO])
 
-call(['docker', 'run', '-d', '-it', '-p', '5000:5000', '--name', 'Course_System_flask_uwsgi',
-      '-v', '/root/Course_System/Back_end_Flask:/app',
-      '-v', '/root/Course_System/config/uwsgi.ini:/app/uwsgi.ini',
-      'flask_uwsgi:latest'])
+    call(['docker', 'rm', '-f', C_NGINX])
+
+
+def run():
+    call(['docker', 'run', '-d', '-p', '80:80', '--name', C_NGINX,
+          '-v', COURSE_SYSTEM + 'Front_end_Vue:/app/html',
+          '-v', COURSE_SYSTEM + 'config/nginx.conf:/etc/nginx/nginx.conf',
+          '-v', COURSE_SYSTEM + 'logs:/var/log/nginx',
+          'nginx:1.16'])
+
+    call(['docker', 'run', '-d', '-p', '5000:5000', '--name', C_DJANGO,
+          '-v', COURSE_SYSTEM + 'Back_end_Django:/app',
+          '-v', COURSE_SYSTEM + 'config/uwsgi.ini:/app/uwsgi.ini',
+          'uwsgi_django:latest'])
+
+
+if __name__ == '__main__':
+    c = input('1. 运行容器组\n2. 删除容器组\n')
+    if c in (2, '2'):
+        rm()
+    else:
+        rm()
+        run()
